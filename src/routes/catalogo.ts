a3537625fcd3da22186f1797d0220afee9641cd0
BlueE-05 <danaetorres35@gmail.com>
2026-03-22 00:14:24 -0600

    feat: add route protection middleware to catalogo routes

--- src/routes/catalogo.ts
+++ src/routes/catalogo.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
 import * as catalogoController from "@/controllers/catalogo";
+import { protectRoute } from "@/middlewares/auth";
 
 const router = Router();
+
+router.use(protectRoute);
 
 router.get("/catalogo/semestre", catalogoController.listSemestres);
 
