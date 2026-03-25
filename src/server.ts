40fb38117742bfc54136ca3fa8784326429a2e82
BlueE-05 <danaetorres35@gmail.com>
2026-03-24 23:00:55 -0600

    feat: add support for URL-encoded data parsing in Express app

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/", catalogoRoutes);
 app.use("/", authRoutes);
