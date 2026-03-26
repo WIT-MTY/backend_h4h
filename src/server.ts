4d2ea763b544d0997b9e19e0d7b5e787a177c583
Khao <fatimacastilloag@gmail.com>
2026-03-25 18:40:23 -0600

    cambio temporal en src/server.ts para una prueba

--- src/server.ts
+++ src/server.ts
@@ -1,34 +1,23 @@
 
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors";  
-import catalogoRoutes from "@/routes/catalogo";
-import authRoutes from "@/routes/auth";
+import catalogoRoutes from "./routes/catalogo.js";
+import authRoutes from "./routes/auth.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({
-    origin: [
-        'http://localhost:3000',
-        'http://192.168.1.14:3000',
-    ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-}));
-
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-app.use("/api", authRoutes);     
-app.use("/api", catalogoRoutes);  
-
+app.use("/", catalogoRoutes);
+app.use("/", authRoutes);
 app.get("/", (req, res) => res.send("API Running"));
 
-app.listen(PORT, () => console.log(`Server on ${PORT}`));
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => console.log(`Server on ${PORT}`));
+}
 
 export default app;
