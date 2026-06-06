fda8ecf27f09cf6b931673067517edad3da9bbc8
Khao <fatimacastilloag@gmail.com>
2026-06-05 18:34:28 -0600

    update endpoint /equipos/:equipo_id/integrantes/:usuario_base_id

--- src/routes/equipos.ts
+++ src/routes/equipos.ts
@@ -14,7 +14,7 @@ router.get("/retos/porEquipo", EquipoController.getRetosElegidosPorEquipo);
 router.post("/create", EquipoController.createTeam);
 router.post("/join/:equipo_codigo_entrada", EquipoController.joinTeam);
 router.get("/myteam", EquipoController.getMyTeam); // obtener equipo si el usuario es líder o participante o si no pertenece a ningún equipo
-router.delete("/equipos/:equipo_id/integrantes/:usuario_base_id", protectRoute, removeIntegranteFromEquipo);
+router.delete("/:equipo_id/integrantes/:usuario_base_id", protectRoute, removeIntegranteFromEquipo);
 // router.post("/leave", protectRoute, teamController.leaveTeam);
 router.delete("/delete/:equipo_id", protectRoute, EquipoController.deleteTeam);
 
